e0ceabbc1cd336f945282f99c9ffbe857eb0ea67
luminousbrain <solo.kutuzov@yandex.ru>
2021-06-14 13:47:31 +0300

    init project

---
+++ src/main/java/ru/gontarenko/codesharingplatform/controller/HomeController.java
@@ -0,0 +1,13 @@
+package ru.gontarenko.codesharingplatform.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping(value = {"/home", "/"})
+    public String home() {
+        return "home";
+    }
+}
